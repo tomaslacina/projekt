b79815d5819f4f39a7dee625ee7071f92a4b6f6b
Stanislav REpka <xrepka@mendelu.cz>
2020-12-27 18:09:02 +0100

    update trener

--- KOPIE PROJEKTU/Projekt/src/main/java/cz/mendelu/pef/pjj/projekt/dostihy/Trener.java
+++ KOPIE PROJEKTU/Projekt/src/main/java/cz/mendelu/pef/pjj/projekt/dostihy/Trener.java
@@ -3,11 +3,20 @@ package cz.mendelu.pef.pjj.projekt.dostihy;
 public class Trener {
     private int cisloPolicka;
     private String text;
+    private boolean obsadenost;
 
 
     Trener(int cisloPolicka, String text){
         this.cisloPolicka=cisloPolicka;
         this.text=text;
+    }
+
+    public int getCisloPolicka() {
+        return cisloPolicka;
+    }
+
+    public boolean isObsadenost() {
+        return obsadenost;
     }
 
     @Override
